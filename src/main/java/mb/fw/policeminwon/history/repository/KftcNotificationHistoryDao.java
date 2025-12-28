b65f2996a25d738d7b44d7f73c9827b144274d2b
nonow <nonow@nonowngh>
2025-12-28 20:41:50 +0900

    인더홈~  - 소스정리

--- src/main/java/mb/fw/policeminwon/history/repository/KftcNotificationHistoryDao.java
+++ src/main/java/mb/fw/policeminwon/history/repository/KftcNotificationHistoryDao.java
@@ -24,9 +24,7 @@ public class KftcNotificationHistoryDao {
 	 * 통지 이력 등록
 	 */
 	public int insert(KftcNotificationHistory entity) {
-
 //		String sql = "INSERT INTO KFTC_NOTIFICATION_HISTORY (PROCESS_DATE, KFTC_MESSAGE_NO, ELECTRONIC_PAYMENT_NO, ESB_TX_ID) VALUES (TO_CHAR(SYSDATE, 'YYYYMMDD'), ?, ?, ?)";
-
 		return jdbcTemplate.update(historyProperties.getInsertSql(), entity.getKftcMessageNo(),
 				entity.getElectronicPaymentNo(), entity.getEsbTxId());
 	}
@@ -35,9 +33,7 @@ public class KftcNotificationHistoryDao {
 	 * 처리일자 + 센터번호 단건 조회
 	 */
 	public KftcNotificationHistory findOne(String kftcMessageNo) {
-
 //		String sql = "SELECT PROCESS_DATE, KFTC_MESSAGE_NO, ELECTRONIC_PAYMENT_NO, ESB_TX_ID FROM KFTC_NOTIFICATION_HISTORY WHERE PROCESS_DATE = TO_CHAR(SYSDATE, 'YYYYMMDD') AND KFTC_MESSAGE_NO = ?";
-
 		List<KftcNotificationHistory> list = jdbcTemplate.query(historyProperties.getSelectSql(),
 				(rs, rowNum) -> new KftcNotificationHistory(rs.getString("PROCESS_DATE"),
 						rs.getString("KFTC_MESSAGE_NO"), rs.getString("ELECTRONIC_PAYMENT_NO"),
